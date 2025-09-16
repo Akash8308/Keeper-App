27b7bcb3ac43ce24bc70febd280c39507180f5ca
Akash <86683095+Akash8308@users.noreply.github.com>
2025-09-16 23:32:56 +0530

    Completed Keeper App
    Completed Keeper App which is notes app

--- Kepper App/src/components/App.jsx
+++ Kepper App/src/components/App.jsx
@@ -5,11 +5,38 @@ import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App() {
+  const [notes, setNotes] = React.useState([]);
+
+  function addNote(newNote) {
+    setNotes((prevvalue) => {
+      return [...prevvalue, newNote];
+    });
+    console.log(notes);
+  }
+
+  function onDelete(id) {
+    setNotes((prevValue) => {
+      return prevValue.filter((note, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div>
       <Header />
-      <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+      <CreateArea onAdd={addNote} />
+      {notes.map((note, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={note.title}
+            content={note.content}
+            onDelete={onDelete}
+          />
+        );
+      })}
       <Footer />
     </div>
   );
--- Kepper App/src/components/CreateArea.jsx
+++ Kepper App/src/components/CreateArea.jsx
@@ -1,12 +1,56 @@
 import React from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+  const [note, setNote] = React.useState({ title: "", content: "" });
+
+  function handleTitleTextChange(event) {
+    const { value, name } = event.target;
+    setNote((prevValue) => {
+      return {
+        ...prevValue,
+        [name]: value,
+      };
+    });
+  }
+
+  function handleContentTextChange(event) {
+    const { value } = event.target;
+    setNote((prevValue) => {
+      return {
+        title: prevValue.title,
+        content: value,
+      };
+    });
+  }
+
+  function handleAddClick(event) {
+    props.onAdd(note);
+    setNote({
+      title: "",
+      content: "",
+    });
+    event.preventDefault();
+  }
+
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input
+          name="title"
+          placeholder="Title"
+          value={note.title}
+          onChange={handleTitleTextChange}
+        />
+        <textarea
+          name="content"
+          placeholder="Take a note..."
+          rows="3"
+          value={note.content}
+          onChange={handleContentTextChange}
+        />
+        <button type="Submit" onClick={handleAddClick}>
+          Add
+        </button>
       </form>
     </div>
   );
